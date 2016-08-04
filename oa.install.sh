b6f87c64396a44db6aa89ea1cf8f62f06b91247c
Aleksey Slyunchenko <alexnovosib88@gmail.com>
2016-08-04 16:55:03 +0700

    update for 7.0

--- oa.install.sh
+++ oa.install.sh
@@ -2,17 +2,33 @@
 ############### OSA installation shell script
 ############### run it on LINMN
 
-############ Should be customized
-PCPPASSWD=secret1
-MNIP=MN.BACK.NET.IP
-MNFQDN=MN.HOSTN.AME
-POATARURL=http://internal.local/poa-6.0-3517.tar
+usage() { echo "Usage: $0 -p PCPPASSWD -i MNIP -h MNFQDN" 1>&2; exit 1; }
+
+while getopts ":p:i:h:d:" option; do
+    case "${option}" in
+    	p)
+			PCPPASSWD=${OPTARG}
+			;;
+		i)
+			MNIP=${OPTARG}
+			;;
+		h)
+			MNFQDN=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $(($OPTIND - 1))
+if [ -z "${PCPPASSWD}" ] || [ -z "${MNIP}" ] || [ -z "${MNFQDN}" ]; then
+    usage
+fi
 
 ############ Optionally customized
 DISTR=/root/distr
-MNMODULES="Linux Shared, APS"
-# set below to True, if resintall is needed
-REINSTALL=False
+POATARURL=https://pa:padownload@download.automation.odin.com/oa/7.0/7.0-release/oa-7.0-9781.tar
+MNMODULES="Linux Shared, APS, PBA"
 
 ############ Do not touch below
 POATAR=$DISTR/${POATARURL##*/}
@@ -20,35 +36,33 @@ POADIR=${POATAR%.tar}
 INSTALLPY=${POADIR}/install.py
 PAUPDATER=/usr/local/pem/bin/pa_updates_installer
 
-if [ $REINSTALL == "True" ]; then
-	REINSTALL_SWITCH="--reinstall"
-else
-	REINSTALL_SWITCH=""
+# download OA distribution
+if [ ! -d "$DISTR" ]; then
+	mkdir $DISTR
 fi
 
-# stop on errors
-set -e
-
-# download OA distribution
-mkdir -p $DISTR
-wget --continue --directory-prefix=$DISTR $POATARURL
-tar --extract --file=$POATAR --directory=$DISTR
+if [ ! -f "$DISTR/oa-7.0-9781.tar" ]; then
+	wget --no-check-certificate --continue --directory-prefix=$DISTR $POATARURL
+	tar --extract --file=$POATAR --directory=$DISTR
+fi
 
 ###### prereqs
 # remove uncompatible java rpms
-rpm -qa '*openjdk*' 'jdk' | xargs --no-run-if-empty rpm -e
+rpm -qa '*openjdk*' 'jdk' | xargs rpm -e
 # update OS
 yum update -y
 # make sure MN hostname resolves propery
-echo $MNIP $MNFQDN >> /etc/hosts
+if [ -z "$(grep "$MNIP $MNFQDN" /etc/hosts)" ]; then
+	echo $MNIP $MNFQDN >> /etc/hosts
+fi
+
 
 # run installer
 chmod +x $INSTALLPY
 
-$INSTALLPY --batch $REINSTALL_SWITCH --communication_ip ${MNIP} --external_ip ${MNIP} --modules="${MNMODULES}" --hostname=${MNFQDN} --username=admin --password="${PCPPASSWD}" --email=admin@${MNFQDN} --database_host=${MNIP}
+$INSTALLPY --batch --communication_ip ${MNIP} --external_ip ${MNIP} --modules="${MNMODULES}" --hostname=${MNFQDN} --username=admin --password="${PCPPASSWD}" --email=admin@${MNFQDN} --database_host=${MNIP}
 
-chkconfig httpd on
+systemctl enable httpd.service
 
 # install updates
-${PAUPDATER} --install
-
+#${PAUPDATER} --install
